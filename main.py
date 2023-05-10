7b8910d117d03191b7bb701f5586d497c6000205
Pigioty <Pigiotyreal@gmail.com>
2023-05-10 01:22:21 -0400

    Listening to socket/Server side

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ port = 8000
 
 socket.bind(("", port))
 print(f"Socket binded to {port}")
+
+socket.listen(5)
+print("Socket listening")
+
+while True:
+    conn, addr = socket.accept()
+    print(f"Got a connection from {addr}")
+    
+    conn.send("Connected!".encode())
+    conn.close()
+    
+    break
+    
