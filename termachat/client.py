b66c1cded54f20260ad6257cdfe09a1cf7baaf74
Pigioty <Pigiotyreal@gmail.com>
2023-05-10 01:50:40 -0400

    Username

--- termachat/client.py
+++ termachat/client.py
@@ -6,4 +6,11 @@ port = 8000
 socket.connect(("127.0.0.1", port))
 
 print(socket.recv(1024).decode())
+
+user = input("Choose a username: ")
+socket.send(user.encode())
+
+user = socket.recv(1024).decode()
+print(f"Your username is {user}")
+
 socket.close()
--- termachat/server.py
+++ termachat/server.py
@@ -14,7 +14,27 @@ while True:
     print(f"Got a connection from {addr}")
     
     conn.send("Connected!".encode())
+    
+    user = conn.recv(1024).decode()
+    
+    if len(user) < 3:
+        conn.send("Invalid username, min chars 3".encode())
+        conn.close()
+        continue
+    if len(user) > 35:
+        conn.send("Invalid username, max chars 35".encode())
+        conn.close()
+        continue
+    if user.isalnum() == False:
+        conn.send("Invalid username, alphanumeric only".encode())
+        conn.close()
+        continue
+    if user.isspace() == True:
+        conn.send("Invalid username, no spaces".encode())
+        conn.close()
+        continue
+    
+    conn.send(user.encode())
+    
     conn.close()
-    
     break
-    
